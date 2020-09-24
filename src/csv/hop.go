697b4023a94b91ed05e580b2012dc62c07267433
Ross Merrigan <email@rossmerrigan.com>
2020-09-24 21:41:37 +0200

    cleanup hops data

--- src/csv/hop.go
+++ src/csv/hop.go
@@ -12,24 +12,20 @@ type Hop struct {
 	CoHumuloneLow        string `csv:"Co-Humulone Low"`
 	CoHumuloneHigh       string `csv:"Co-Humulone High"`
 	Country              string `csv:"Country"`
-	ConeSize             string `csv:"Cone Size"`
-	ConeDensity          string `csv:"Cone Density"`
-	SeasonalMaturity     string `csv:"Seasonal Maturity"`
-	YieldAmount          string `csv:"Yield Amount"`
-	GrowthRate           string `csv:"Growth Rate"`
-	Resistantto          string `csv:"Resistant to"`
-	Susceptibleto        string `csv:"Susceptible to"`
 	Storability          string `csv:"Storability"`
-	EaseofHarvest        string `csv:"Easeof Harvest"`
 	TotalOilComposition  string `csv:"Total Oil Composition"`
-	MyrceneOilLow        string `csv:"Myrcene Oil Low"`
-	MyrceneOilHigh       string `csv:"Myrcene Oil High"`
+	MyrceneOilLow        string `csv:"Myrcene Oil Low (mL/100g)"`
+	MyrceneOilHigh       string `csv:"Myrcene Oil High (mL/100g)"`
 	HumuleneOilLow       string `csv:"Humulene Oil Low"`
 	HumuleneOilHigh      string `csv:"Humulene Oil High"`
 	CaryophylleneOilLow  string `csv:"Caryophyllene Oil Low"`
 	CaryophylleneOilHigh string `csv:"Caryophyllene Oil High"`
 	FarneseneOilLow      string `csv:"Farnesene Oil Low"`
 	FarneseneOilHigh     string `csv:"Farnesene Oil High"`
+	LinaloolOilLow       string `csv:"Linalool Oil Low"`
+	LinaloolOilHigh      string `csv:"Linalool Oil High"`
+	PolyphenolsOilLow    string `csv:"Polyphenols Oil Low"`
+	PolyphenolsOilHigh   string `csv:"Polyphenols Oil High"`
 	Substitutes          string `csv:"Substitutes"`
 	StyleGuide           string `csv:"Style Guide"`
 	Description          string `csv:"Description"`
